4d06dfd6fd12e8a0ac3bfaadb809853a805dffbb
hisyatokaku <hisyatokaku2005@yahoo.co.jp>
2018-05-18 11:09:19 +0200

    handle input as a vector, change sgd to rmsprop

--- source/model.py
+++ source/model.py
@@ -4,6 +4,7 @@ import numpy as np
 from keras.models import Model
 # from keras.utils import plot_model
 from keras.layers import Input, LSTM, Dense, Embedding, Bidirectional, Flatten, Reshape, Lambda, TimeDistributed, concatenate, multiply
+from keras import optimizers
 from keras.layers.convolutional import Conv1D
 from keras.layers.pooling import MaxPooling2D
 
@@ -15,7 +16,7 @@ class Classifier():
         self.model = None
 
         self.batchsize = 64
-        self.epochs = 5
+        self.epochs = 20
 
         self.n_stories = 4
         self.n_options = 1
@@ -39,27 +40,34 @@ class Classifier():
         embed_layer = TimeDistributed(Embedding(input_dim=self.vocab_size, output_dim=self.embed_dim,
                                                 input_length=self.max_seqlen, mask_zero=True))
 
-        birnn_layer = TimeDistributed(Bidirectional(LSTM(self.hidden_dim)))
+        birnn_layer = TimeDistributed(Bidirectional(LSTM(self.hidden_dim, input_shape=(5, 30, self.embed_dim))))
         dense_layer = TimeDistributed(Dense(self.feature_dim, activation='relu'))
         ending_dense_layer = TimeDistributed(Dense(self.n_stories * self.feature_dim, activation='relu'))
 
         embeddings = embed_layer(inputs) # (None, 5, 30) -> (None, 5, 30, output_dim)
-        birnn_outputs = birnn_layer(embeddings) # (None, 5, 30, output_dim) -> (None, 5, hidden_dim)
+        print('embeddings.shape: ', embeddings.shape)
 
-        story_outputs = Lambda(lambda x: x[:, :4, :], output_shape=(4, self.hidden_dim))(birnn_outputs)
-        ending_outputs = Lambda(lambda x: x[:, 4:, :], output_shape=(1, self.hidden_dim))(birnn_outputs)
+        birnn_outputs = birnn_layer(embeddings) # (None, 5, 30, output_dim) -> (None, 5, hidden_dim)
+        print('birnn_outputs.shape: ', birnn_outputs.shape)
+
+        story_outputs = Lambda(lambda x: x[:, :4, :], output_shape=(4, 2 * self.hidden_dim))(birnn_outputs)
+        ending_outputs = Lambda(lambda x: x[:, 4:, :], output_shape=(1, 2 * self.hidden_dim))(birnn_outputs)
+        # memo: bidirectional LSTM returns the vector with 2 * self.hidden row size.
+        #       when applying lambda layer, if you forget 2 * in output_shape, the batchsize will be doubled instead,
+        #       which will cause the error later line.
 
         fc_outputs = dense_layer(story_outputs) # (None, 4, hidden_num) -> (None, 4, feature_dim)
         ending_features = ending_dense_layer(ending_outputs) # (None, 1, hidden_num) -> (None, 1, 4 * feature_num)
-        story_features = Reshape((1, 4 * self.feature_dim))(fc_outputs)
+        ending_features = Flatten()(ending_features) # TODO: check this flattening works as expected
+        # story_features = Reshape((1, 4 * self.feature_dim))(fc_outputs)
+        story_features = Flatten()(fc_outputs)
         story_features = multiply([story_features, ending_features])  # TODO make it more exact like paper do
-        story_features = Flatten()(story_features)
-        #         conv = Conv1D(16, kernel_size=3, activation='relu')(story_features)
+
         fc = Dense(1, activation='sigmoid')(story_features)
 
-        print('fc.shape: ', fc.shape)
         model = Model(inputs=[story_inputs, option_input], outputs=fc)
-        model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+        rmsprop = optimizers.RMSprop()
+        model.compile(optimizer=rmsprop, loss='binary_crossentropy', metrics=['accuracy'])
 
         # plot_model(model, Path.image_save_path)
         print(model.summary())
@@ -68,9 +76,8 @@ class Classifier():
     def train(self, inputs, outputs):
         if self.model == None:
             raise ValueError("self.model is None. run build_model() first.")
-
         hist = self.model.fit(inputs, outputs, epochs=self.epochs, batch_size=self.batchsize,
-                              shuffle=True, validation_split=0.2, verbose=2, class_weight=self.class_weight)
+                              shuffle=True, validation_split=0.2, verbose=1, class_weight=self.class_weight)
         # model.fit() returns history object which contains all the validation scores and so on.
         return hist
 
--- source/run_experiment.py
+++ source/run_experiment.py
@@ -8,7 +8,7 @@ save_model_path = datetime.now().strftime('%m%d%Y_%H%M')
 save_model_path = os.path.join(Path.model_save_path, save_model_path)
 
 def main():
-    data = Data(data_limit=100)
+    data = Data(data_limit=None)
     print("data loaded.")
 
     classifier = Classifier(max_seqlen = data.max_seqlen, vocab_size = data.vocab_size, n_dummy = data.n_dummy)
@@ -23,14 +23,16 @@ def main():
     classifier.train(inputs, answers)
 
     # test
-    test_inputs_with_1 = [data.test_x[:, i, :] for i in range(4)] + [data.test_e1[:, 0, :]]
-    test_inputs_with_2 = [data.test_x[:, i, :] for i in range(4)] + [data.test_e2[:, 0, :]]
+    # test_inputs_with_1 = [data.test_x[:, i, :] for i in range(4)] + [data.test_e1[:, 0, :]]
+    # test_inputs_with_2 = [data.test_x[:, i, :] for i in range(4)] + [data.test_e2[:, 0, :]]
+    test_inputs_with_1 = [data.test_x[:, :4, :], data.test_e1]
+    test_inputs_with_2 = [data.test_x[:, :4, :], data.test_e2]
 
     answer_with_1 = classifier.test(test_inputs_with_1, batchsize=32)
     answer_with_2 = classifier.test(test_inputs_with_2, batchsize=32)
 
     acc = classifier.calculate_accuracy(answer_with_1, answer_with_2, gt=data.test_answers)
-    print("test acc: {}".format(acc))
+    print("test acc: {}".format(acc/float(data.test_x.shape[0])))
 
     print("model saving...")
     if not os.path.exists(save_model_path):
