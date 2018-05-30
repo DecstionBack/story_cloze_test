8b5f42944e4f10206963884926482d0135501b48
hisyatokaku <hisyatokaku2005@yahoo.co.jp>
2018-05-30 16:13:49 +0200

    add new data from alex

--- source/path.py
+++ source/path.py
@@ -1,6 +1,6 @@
 class Path:
-    train_file_path = "../data/train_stories.csv"
-    # train_file_path = "../data/storydata_all.csv"
+    # train_file_path = "../data/train_stories.csv"
+    train_file_path = "../data/storydata.csv"
     val_file_path = "../data/val_stories.csv"
     image_save_path = "../image/model.png"
     model_save_path = "../model/"
